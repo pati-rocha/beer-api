2f81de4dd9a7c51ddec45f5315cb8d8f9c4787df
Pati Rocha <patirocha.dev@gmail.com>
2023-04-28 00:49:46 -0300

    feat: add providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformResponseInterceptor } from './core/http/transform.response.interceptor';
+import { Database } from './database/database';
+import { BeerModule } from './beer/beer.module';
 
 @Module({
-  imports: [],
+  imports: [BeerModule],
   controllers: [],
-  providers: [],
+  providers: [
+    Database,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
